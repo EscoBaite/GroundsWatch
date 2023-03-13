7996c38fc51626a6c903e2a30622a280196b7f5a
EscoBaite <mwitiben@gmail.com>
2023-03-13 13:53:28 -0600

    Form to make new eventgrouds and its routes and page connections

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override');
 const Eventground = require('./models/eventground')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/groundswatch', { useCreateIndex: true, useUnifiedTopology: true,  useNewUrlParser: true })
     .then(() => {
@@ -23,6 +28,16 @@ app.get('/', (req, res) => {
 app.get('/eventgrounds', async (req, res) => {
     const eventgrounds = await Eventground.find({})
     res.render('eventgrounds/index.ejs', { eventgrounds })
+})
+
+app.get('/eventgrounds/new', (req, res) => {
+    res.render('eventgrounds/new');
+})
+
+app.post('/eventgrounds', async (req, res) => {
+    const eventground = new Eventground(req.body.eventground);
+    await eventground.save();
+    res.redirect(`/eventgrounds/${eventground._id}`)
 })
 
 app.get('/eventgrounds/:id', async (req, res) => {
