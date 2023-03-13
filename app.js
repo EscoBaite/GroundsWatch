b7cd022ba5c53635b6d789a669898e2bc9f119b1
EscoBaite <mwitiben@gmail.com>
2023-03-13 15:48:03 -0600

    Added and configured layous with ejs_Mate

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
-const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate')
+const methodOverride = require('method-override')
 const Eventground = require('./models/eventground')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.engine('ejs', ejsMate)
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/groundswatch', { useCreateIndex: true, useUnifiedTopology: true,  useNewUrlParser: true })
