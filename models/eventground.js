d2917c50b2c57c22272e492da35447957a8da32a
EscoBaite <mwitiben@gmail.com>
2023-03-16 01:28:42 +0300

    Delete event grounds middleware

--- models/eventground.js
+++ models/eventground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Review = require('./review')
 const Schema = mongoose.Schema
 
 const EventgroundSchema = new Schema({
@@ -14,6 +15,16 @@ const EventgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+})
+
+EventgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Eventground', EventgroundSchema)
