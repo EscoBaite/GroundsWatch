6eb9a0175c2c7281fcac5fcb083df3ed98acc06e
EscoBaite <mwitiben@gmail.com>
2023-03-21 17:56:57 +0300

    recentering map

--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
 container: 'cluster-map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/light-v11',
-center: [-103.5917, 40.6699],
+center: [37.6456, 0.0515],
 zoom: 3
 });
 
